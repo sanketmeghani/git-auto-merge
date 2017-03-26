e049df01230a80f2ba89f28932c78856b5789779
Sanket Meghani <sanket.meghani@gmail.com>
2017-03-26 10:12:40 +0530

    Adding commands to checkout branch

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ let git = simpleGit('../test-repo');
 let checkoutAPI = (error, response) => {
 
 	if(error) {
-		console.log('Error checking out branch', error);
+		console.log('Unable to checkout branch develop.api', error);
 		console.log('Response', response);
 	}else {
-		console.log('Success response', response);
+		console.log('Checked out branch develop.api');
 	}
 };
 
-git().checkout('develop.api', checkoutAPI);
+git.checkout('develop.api', checkoutAPI);
